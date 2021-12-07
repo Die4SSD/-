87f0ee6493c130b642d1e85952f3dafb9e23aadc
Die4SSD <93977827+Die4SSD@users.noreply.github.com>
2021-12-07 13:04:54 +0300

    Add files via upload

--- weather/weather.js
+++ weather/weather.js
@@ -1,22 +1,30 @@
 import {Weather} from "./weatherClass.js"
-const getWidget = new Weather
+//const getWidget = new Weather
 async function getWeatherWidget() {
-    const weather = await new getWidget('Minsk').getWeather()
+    const weather = await new Weather('Minsk').getWeather()
     let temperature = Math.round(weather.weather.main.temp-273,15)
     let cityName = weather.weather.name
     let weatherIcon = weather.weather.weather[0].icon
     let weatherDescription = weather.weather.weather[0].description
     let weatherWind = weather.weather.wind.speed
     function toHTML() {
-        document.getElementById('bodyID').innerHTML += `<div id='divID'><p>Сейчас в ${cityName}</p>
+        document.getElementById('bodyID').innerHTML += `<div id='divID'>
+                                                        <input type = button class= closebut value = "X" onclick="this.parentElement.style.display = 'none';">
+                                                        <p>Сейчас в ${cityName}</p>
                                                         <p> ${temperature} °C ${weatherDescription}</p>
                                                         <p>Ветер: ${weatherWind} м/с</p>
                                                         <img src ="http://openweathermap.org/img/wn/${weatherIcon}@2x.png">
-                                                        <h3 id="close" onclick="this.parentElement.style.display = 'none';">X</h3>
-                                                        <input type = button id= button value = "Прогноз на 3 дня" onclick=>
+                                                        <input type = button id= 'buttonShow' value = "Прогноз на 3 дня" onclick=>
                                                         <div>`
     }
     toHTML();
+    let btn =  document.getElementById('buttonShow')
+    function call(){
+        getWeatherWidget5Day();
+        getWeatherWidget5DayTomorrow();
+        getWeatherWidget5DayDoubleTomorrow();
+    }
+    btn.onclick = call
 }
 async function getWeatherWidget5Day(){
     const weather5Days = await new Weather('Minsk').getWeather5Days()
@@ -26,11 +34,12 @@ async function getWeatherWidget5Day(){
     let weatherDescription = (weather5Days.weather5Days.list[0].weather[0].description)
     let weatherWind = (weather5Days.weather5Days.list[0].wind.speed)
     function toHTML() {
-        document.getElementById('bodyID').innerHTML += `<div id='divID2'><p>Сегодня в ${cityName}</p>
+        document.getElementById('bodyID').innerHTML += `<div id='divID2'>
+                                                        <input type = button class= closebut value = "X" onclick="this.parentElement.style.display = 'none';">
+                                                        <p>Сегодня в ${cityName}</p>
                                                         <p> ${temperature} °C ${weatherDescription}</p>
                                                         <p>Ветер: ${weatherWind} м/с</p>
                                                         <img src ="http://openweathermap.org/img/wn/${weatherIcon}@2x.png">
-                                                        <h3 id="close" onclick="this.parentElement.style.display = 'none';">X</h3>
                                                         <div>`
     }
     toHTML()
@@ -43,11 +52,12 @@ async function getWeatherWidget5DayTomorrow(){
     let weatherDescription = (weather5Days.weather5Days.list[7].weather[0].description)
     let weatherWind = (weather5Days.weather5Days.list[7].wind.speed)
     function toHTML() {
-        document.getElementById('bodyID').innerHTML += `<div id='divID3'><p>Завтра в ${cityName}</p>
+        document.getElementById('bodyID').innerHTML += `<div id='divID3'>
+                                                        <input type = button class= closebut value = "X" onclick="this.parentElement.style.display = 'none';">
+                                                        <p>Завтра в ${cityName}</p>
                                                         <p> ${temperature} °C ${weatherDescription}</p>
                                                         <p>Ветер: ${weatherWind} м/с</p>
                                                         <img src ="http://openweathermap.org/img/wn/${weatherIcon}@2x.png">
-                                                        <h3 id="close" onclick="this.parentElement.style.display = 'none';">X</h3>
                                                         <div>`
     }
     toHTML()
@@ -60,11 +70,12 @@ async function getWeatherWidget5DayDoubleTomorrow(){
     let weatherDescription = (weather5Days.weather5Days.list[14].weather[0].description)
     let weatherWind = (weather5Days.weather5Days.list[14].wind.speed)
     function toHTML() {
-        document.getElementById('bodyID').innerHTML += `<div id='divID4'><p>Послезавтра в ${cityName}</p>
+        document.getElementById('bodyID').innerHTML += `<div id='divID4'>
+                                                        <input type = button class= closebut value = "X" onclick="this.parentElement.style.display = 'none';">
+                                                        <p>Послезавтра в ${cityName}</p>
                                                         <p> ${temperature} °C ${weatherDescription}</p>
                                                         <p>Ветер: ${weatherWind} м/с</p>
                                                         <img src ="http://openweathermap.org/img/wn/${weatherIcon}@2x.png">
-                                                        <h3 id="close" onclick="this.parentElement.style.display = 'none';">X</h3>
                                                         <div>`
     }
     toHTML()
